edcbdde99a2e504d2eff0a44453cda00855fab9b
Chase Wilson <31453523+chasewilson@users.noreply.github.com>
2025-10-29 22:23:46 -0700

    corrections to imports and main

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
+
+	"github.com/chasewilson/chaos-proxy/internal/config"
 )
 
 func main() {
 	configFile := flag.String("config", "", "path to config file")
 	flag.Parse()
 
+	if *configFile == "" {
+		log.Fatal("config file path is required")
+	}
+
+	routeConfigs, err := config.LoadConfig(*configFile)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	fmt.Println(routeConfigs)
 }
